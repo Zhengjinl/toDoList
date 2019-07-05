11826aa5ae584cb1e61ac4f644c561ec3cfe966f
singlepingguo <unknown@example.com>
2019-07-05 17:35:02 +0800

    首页提交
    编辑首页样式，提交代码

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,20 @@
 //index.js
+var util = require('../../utils/util.js');  
 //获取应用实例
 const app = getApp()
-console.error('app===>',app)
 
 Page({
   data: {
-    motto: '创建微信小程序项目',
+    wHeight: '',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    time: '',
+    todoList:[
+      {'title':'重要不紧急','cont': ['今天你吃饭了吗', '有个地方还没去', '哈哈哈哈哈哈哈', '不变速百度搜不是']},
+      { 'title': '重要且紧急', 'cont': ['拉阿拉啦啦啦啦啦', '少时诵诗书所', '第三方第三方而定', '第三次发生大V从', '拉阿拉啦啦啦啦啦', '少时诵诗书所', '第三方第三方而定', '啦啦啦啦', '拉阿拉啦啦啦啦啦', '少时诵诗书所', '第三方第三方而定']},
+      { 'title': '不重要不紧急','cont': ['不重要但是很紧急', '什么事没什么什么事', '第嘎嘎嘎嘎嘎过过过', '少吃点饭']},
+      { 'title': '不重要但紧急','cont':['不重要紧急','有时间做就好','其他任务要优先做','哈哈哈哈']}
+    ]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -18,31 +24,37 @@ Page({
     })
   },
   onLoad: function () {
-    console.error('第一个判断',app.globalData.userInfo)
-    // app.globalData.userInfo = null;
-    // this.data.canIUse = null;
+    var that =this
+    var currentTime = util.formatTime(new Date()).split(" ");
+    console.error(currentTime)
+    var height = wx.getSystemInfoSync().windowHeight; 
+    that.setData({
+      wHeight: height,
+      ['time.data']: currentTime[0],
+      ['time.week']: currentTime[1]
+    });  
+    console.error(app.globalData.userInfo)
     if (app.globalData.userInfo) {
-      this.setData({
+      that.setData({
         userInfo: app.globalData.userInfo,
+        ['userInfo.job']: '前端工程师',
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
-      console.error('第二个判断条件')
+    } else if (that.data.canIUse){
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        this.setData({
+        that.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
       }
     } else {
-      console.error('第三个判断条件')
       // 在没有 open-type=getUserInfo 版本的兼容处理
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
-          this.setData({
+          that.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
@@ -50,12 +62,12 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
-    console.error(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  // getUserInfo: function(e) {
+  //   console.error(e)
+  //   app.globalData.userInfo = e.detail.userInfo
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   })
+  // }
 })
--- utils/util.js
+++ utils/util.js
@@ -5,13 +5,40 @@ const formatTime = date => {
   const hour = date.getHours()
   const minute = date.getMinutes()
   const second = date.getSeconds()
-
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  let week = date.getDay()
+  
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [week].map(turnWeek) + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
+}
+const turnWeek = week =>{
+  switch (week) {
+    case 1:
+      week = '星期一';
+      break;
+    case 2:
+      week = '星期二';
+      break;
+    case 3:
+      week = '星期三';
+      break;
+    case 4:
+      week = '星期四';
+      break;
+    case 5:
+      week = '星期五';
+      break;
+    case 6:
+      week = '星期六';
+      break;
+    case 0:
+      week = '星期日';
+      break;
+  }
+  return week
 }
 
 module.exports = {
